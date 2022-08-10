15897176f387d383224f34ffc725822aff65f634
Aman Srivastva <2019umt1841@mnit.ac.in>
2022-08-11 02:39:37 +0530

    first commit

---
+++ Aman/script.js
@@ -0,0 +1,8 @@
+// Navbar JavaScript
+
+function toggleMenu (){
+    var menutoggle = document.querySelector ('.toggle');
+    var menu = document.querySelector ('.menu');
+    menutoggle.classList.toggle('active')
+    menu.classList.toggle('active')
+}
